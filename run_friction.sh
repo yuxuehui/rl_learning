b793027b9f8466e2531102fb13b47d2c24856404
loxs <1043694812@qq.com>
2023-10-19 14:28:15 +0800

    update

--- run_friction.sh
+++ run_friction.sh
@@ -1,38 +1,10 @@
 
-nohup python panda_sac.py --domain_name PandaPush-v3 --test_lateral_friction 1 > PandaPush_friction_1.out &
+nohup python panda_sac.py --domain_name PandaPush-v3 --test_lateral_friction 2 > PandaPush_friction_2.out &
 
-nohup python panda_sac.py --domain_name PandaPushDense-v3 --test_lateral_friction 1 > PandaPushDense_friction_1.out &
+nohup python panda_sac.py --domain_name PandaPush-v3 --test_lateral_friction 5 > PandaPush_friction_5.out &
 
-nohup python panda_sac.py --domain_name PandaPickAndPlace-v3 --test_lateral_friction 1 > PandaPickAndPlace_friction_1.out &
+nohup python panda_sac.py --domain_name PandaPush-v3 --test_lateral_friction 10 > PandaPush_friction_10.out &
 
-nohup python panda_sac.py --domain_name PandaPickAndPlaceDense-v3 --test_lateral_friction 1 > PandaPickAndPlaceDense_friction_1.out &
+nohup python panda_sac.py --domain_name PandaPush-v3 --test_lateral_friction 15 > PandaPush_friction_15.out &
 
-
-nohup python panda_sac.py --domain_name PandaPush-v3 --test_lateral_friction 20 > PandaPush_friction_20.out &
-
-nohup python panda_sac.py --domain_name PandaPushDense-v3 --test_lateral_friction 20 > PandaPushDense_friction_20.out &
-
-nohup python panda_sac.py --domain_name PandaPickAndPlace-v3 --test_lateral_friction 20 > PandaPickAndPlace_friction_20.out &
-
-nohup python panda_sac.py --domain_name PandaPickAndPlaceDense-v3 --test_lateral_friction 20 > PandaPickAndPlaceDense_friction_20.out &
-
-
-nohup python panda_sac.py --domain_name PandaPush-v3 --test_lateral_friction 40 > PandaPush_friction_40.out &
-
-nohup python panda_sac.py --domain_name PandaPushDense-v3 --test_lateral_friction 40 > PandaPushDense_friction_40.out &
-
-nohup python panda_sac.py --domain_name PandaPickAndPlace-v3 --test_lateral_friction 40 > PandaPickAndPlace_friction_40.out &
-
-nohup python panda_sac.py --domain_name PandaPickAndPlaceDense-v3 --test_lateral_friction 40 > PandaPickAndPlaceDense_friction_40.out &
-
-
-nohup python panda_sac.py --domain_name PandaPush-v3 --test_lateral_friction 60 > PandaPush_friction_60.out &
-
-nohup python panda_sac.py --domain_name PandaPushDense-v3 --test_lateral_friction 60 > PandaPushDense_friction_60.out &
-
-nohup python panda_sac.py --domain_name PandaPickAndPlace-v3 --test_lateral_friction 60 > PandaPickAndPlace_friction_60.out &
-
-nohup python panda_sac.py --domain_name PandaPickAndPlaceDense-v3 --test_lateral_friction 60 > PandaPickAndPlaceDense_friction_60.out &
-
-#nohup python panda_sac.py --domain_name PandaPickAndPlace-v3 --time_step 2000000 > PandaPickAndPlace_round2e6.out &
-
+nohup python panda_sac.py --domain_name PandaPush-v3 --test_lateral_friction 15 > PandaPush_friction_20.out &
---
+++ run_height_test.sh
@@ -0,0 +1,40 @@
+
+nohup python panda_sac.py --domain_name PandaPush-v3 --test_lateral_friction 1 --test_object_height 0.2 > PandaPush_friction_1_height0.2.out &
+
+nohup python panda_sac.py --domain_name PandaPickAndPlace-v3 --test_lateral_friction 1 --test_object_height 0.2 > PandaPickAndPlace_friction_1_height0.2.out &
+
+nohup python panda_sac.py --domain_name PandaPush-v3 --test_lateral_friction 60 --test_object_height 0.2 > PandaPush_friction_60_height0.2.out &
+
+nohup python panda_sac.py --domain_name PandaPickAndPlace-v3 --test_lateral_friction 60 --test_object_height 0.2 > PandaPickAndPlace_friction_60_height0.2.out &
+
+nohup python panda_sac.py --domain_name PandaPush-v3 --test_lateral_friction 0 --test_object_height 0.2 > PandaPush_friction_0_height0.2.out &
+
+nohup python panda_sac.py --domain_name PandaPickAndPlace-v3 --test_lateral_friction 0 --test_object_height 0.2 > PandaPickAndPlace_friction_0_height0.2.out &
+
+
+
+nohup python panda_sac.py --domain_name PandaPush-v3 --test_lateral_friction 1 --test_object_height 0.5 > PandaPush_friction_1_height0.5.out &
+
+nohup python panda_sac.py --domain_name PandaPickAndPlace-v3 --test_lateral_friction 1 --test_object_height 0.5 > PandaPickAndPlace_friction_1_height0.5.out &
+
+nohup python panda_sac.py --domain_name PandaPush-v3 --test_lateral_friction 60 --test_object_height 0.5 > PandaPush_friction_60_height0.5.out &
+
+nohup python panda_sac.py --domain_name PandaPickAndPlace-v3 --test_lateral_friction 60 --test_object_height 0.5 > PandaPickAndPlace_friction_60_height0.5.out &
+
+nohup python panda_sac.py --domain_name PandaPush-v3 --test_lateral_friction 0 --test_object_height 0.5 > PandaPush_friction_0_height0.5.out &
+
+nohup python panda_sac.py --domain_name PandaPickAndPlace-v3 --test_lateral_friction 0 --test_object_height 0.5 > PandaPickAndPlace_friction_0_height0.5.out &
+
+
+nohup python panda_sac.py --domain_name PandaPush-v3 --test_lateral_friction 1 --test_object_height 2.0 > PandaPush_friction_1_height2.0.out &
+
+nohup python panda_sac.py --domain_name PandaPickAndPlace-v3 --test_lateral_friction 1 --test_object_height 2.0 > PandaPickAndPlace_friction_1_height2.0.out &
+
+nohup python panda_sac.py --domain_name PandaPush-v3 --test_lateral_friction 60 --test_object_height 2.0 > PandaPush_friction_60_height2.0.out &
+
+nohup python panda_sac.py --domain_name PandaPickAndPlace-v3 --test_lateral_friction 60 --test_object_height 2.0 > PandaPickAndPlace_friction_60_height2.0.out &
+
+nohup python panda_sac.py --domain_name PandaPush-v3 --test_lateral_friction 0 --test_object_height 2.0 > PandaPush_friction_0_height2.0.out &
+
+nohup python panda_sac.py --domain_name PandaPickAndPlace-v3 --test_lateral_friction 0 --test_object_height 2.0 > PandaPickAndPlace_friction_0_height2.0.out &
+
